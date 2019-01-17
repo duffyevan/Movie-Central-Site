773a4b5905e4cb27188bea159519a0b2e33a2be4
Evan Duffy <elduffy@wpi.edu>
2019-01-17 11:35:18 -0500

    Updated UI

--- index.php
+++ index.php
@@ -9,41 +9,44 @@
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap-theme.min.css">
 	
 	<!-- Dark and Light themes-->
-	<link rel="stylesheet" type="text/css" href="/css/style.css">
+	<link rel="stylesheet" type="text/css" href="/css/new.css">
 	<!-- Latest compiled and minified JavaScript -->
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
 	<!-- jquery for switching themes-->
 	<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
 
 </head>
+<body>
+	<div class="header">
+		<h1 class="headerText">Welcome to Movie Central</h1>
+		<small>
+				<?php
+					$mult=100;
+					echo round(disk_free_space("/Storage/video/")/(1024*1024*1024)*$mult)/$mult . " GB Free On The Disk In Use"
+				?>
+		</small>
+	</div>
 	<div class="container">
-		<center><img style="width:80%" src="/images/movieT.png" style="width:50%"></center>
-		<center><h1 class="yellow">Welcome to Movie Central</h1></center>
-		<center><small class="yellow">
-			<?php
-				$mult=100;
-				echo round(disk_free_space(".")/(1024*1024*1024)*$mult)/$mult . " GB Free On The Disk In Use"
-			?>
-		</small></center>
 		<p style="text-align:center"><br><br></p>
-		<h2 class="yellow">
+		<h2 class="mainText">
 		<?php 
-                $dir = "/var/www/movieCentral/video/";
+                $dir = "/Storage/video/";
                 $files = scandir($dir) or die ("FUCK something went wrong loading the files...");
                 foreach ($files as $file){
                         if ($file != "index.php" && $file != "." && $file != ".." && $file != ".htaccess" && $file != "style.css")
-                            echo "<center><div class='well' style='width: 60%'><center><a class='red' href='video/" . $file . "'>" . str_replace(".mp4", "", str_replace(".mkv", "", str_replace(".avi", "", $file))) . "</a></center></div></center>";
+                            echo "<center><a href='video/" . $file . "'><div class='well'><center>" . str_replace(".mp4", "", str_replace(".mkv", "", str_replace(".avi", "", $file))) . "</center></div></a></center>";
                         }
             ?>
 		</h2>
 	</div>
-	<center class="yellow" style="font-size:12">Site Made By Evan Duffy<br></center>
+	<center class="mainText" style="font-size:12">Site Made By Evan Duffy<br></center>
 	<div class="myfooter">
-        <p class="yellow">March 2016</p>
+        <p class="mainText">December 2018</p>
     </div>
-    <p class="version" style="color:#444444">Version 1.1</p>
+    <p class="version">Version 1.2</p>
 </body>
 </html>
 <!-- CHANGELOG -->
 <!-- 1.0 Initial release, everything works and I'm happy -->
 <!-- 1.1 Made things fancier -->
+<!-- 1.2 Made things slightly less gabage and set it up for the other computer -->
